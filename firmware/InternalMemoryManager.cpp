de624ea3c7b7970d28112b776d1be1344c3a0685
Andrey Sokolov <sokolov19830711@gmail.com>
2020-10-11 18:43:14 +0300

    Добавил возможность сброса устройства до дефолтных настроек

--- firmware/InternalMemoryManager.cpp
+++ firmware/InternalMemoryManager.cpp
@@ -5,6 +5,7 @@
 
 #include "DataManager.h"
 #include "Beeper.h"
+#include "Krakeenone_pinout.h"
 
 InternalMemoryManager::InternalMemoryManager()
 {
@@ -13,7 +14,7 @@ InternalMemoryManager::InternalMemoryManager()
 
 void InternalMemoryManager::initConfig()
 {
-    if (eeprom_read_byte(0) == 123)
+    if (eeprom_read_byte(0) == 123 && digitalRead(DEFAULT_PIN))
     {
         eeprom_read_block((void*)(&DataManager::config()), 1, sizeof(McuInData));
     }
