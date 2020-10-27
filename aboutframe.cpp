d74f32741c19ac27109a264c109dfebcd0a82178
Andrey Sokolov <sokolov19830711@gmail.com>
2020-10-27 10:11:00 +0300

    Изменен внешний вид текста в кадре активации

--- aboutframe.cpp
+++ aboutframe.cpp
@@ -15,7 +15,7 @@ AboutFrame::AboutFrame(QSharedPointer<QSettings> settings, McuInData *mcuInData,
     activationKey_ = new QLineEdit(this);
     activationKey_->setStyleSheet("font-size:14pt;");
     activationKey_->setAlignment(Qt::AlignCenter);
-    activationKey_->setFixedSize(450, 25);
+    activationKey_->setFixedSize(500, 25);
     activationKey_->setText("XXXX-XXXX-XXXX-XXXX");
 
     activateButton_ = new QPushButton(this);
@@ -29,14 +29,16 @@ AboutFrame::AboutFrame(QSharedPointer<QSettings> settings, McuInData *mcuInData,
     mainLayout->addRow(actLayout);
 
     infoText_ = new QPlainTextEdit(this);
-    infoText_->setPlainText("Все  исключительные  права  на  данный  программно-аппаратный  комплекс принадлежат  торговым  маркам  PRIMEBOX  и  TRUEDEVICE."
-                        " Пользователю (или  иным  третьим  лицам)  запрещается  менять,  дизассемблировать, декомпилировать,  "
+    infoText_->setFixedSize(600, 300);
+    infoText_->setStyleSheet("border-style:none; font-size:12pt; color:#ffffff");
+    infoText_->setPlainText("Все  исключительные  права  на  данный  программно-аппаратный  комплекс принадлежат  торговым  маркам  PRIMEBOX  и  TRUEDEVICE. "
+                        "Пользователю (или  иным  третьим  лицам)  запрещается  менять,  дизассемблировать, декомпилировать,  "
                         "дешифровать  или  проводить  другие  действия  с объектным  кодом  и  аппаратной  частью  данного  комплекса  без"
                         " письменного  согласия  Правообладателя.  Пользователь  не  имеет  права распространять  данный  программно-аппаратный  комплекс, "
                         "воспроизводить и доводить ее до всеобщего сведения в какой-либо форме и  способом  без  письменного  согласия  Правообладателя. "
                         "Невыполнение вышеперечисленных  требований  преследуется  административной  и уголовной ответственностью.");
 
-    infoText_->setStyleSheet("font:10pt; color:#ffffff");
     infoText_->setReadOnly(true);
+    mainLayout->addWidget(new QLabel(this));
     mainLayout->addRow(infoText_);
 }
